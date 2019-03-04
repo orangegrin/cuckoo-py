5460b0a72648b57094e4adbe2f7130935fbc8c6a
henry <xbhuang1994@gmail.com>
2019-03-04 16:19:57 +0800

    fix

--- ExchangeService.py
+++ ExchangeService.py
@@ -2,56 +2,48 @@ import asyncio
 import aioredis
 
 
-
 class ExchangeService:
-    def __init__(self,strategy):
-        self.strategy = strategy
 
-    async def subscribeorderbook(self,exchangeName, symbol, strategy_callback):
+    async def subscribeorderbook(self, exchangeName, symbol, strategy_callback):
         channel = exchangeName+".orderbook"+"."+symbol
-        await self.subscribe(channel, callback=self.onSubscribeOrderbook)
+        await self.subscribe(channel, callback=strategy_callback)
 
     def onSubscribeOrderbook(self, channel, data):
         print("on callback")
         print(channel)
         print(data)
-        orderbook = [{1,2}]
-        self.strategy.orderbookchangehandler(orderbook)
+        orderbook = [{1, 2}]
+        self.orderbookchangehandler(orderbook)
 
     def subscribeposition(self, exchangeName, symbol, callback):
         NotImplementedError("subscribeorderbook")
 
-
     def subscribeorderchange(self, exchangeName, symbol, callback):
         NotImplementedError("subscribeorderbook")
 
-
-    def modifylimitorder(self, exchangeName, symbol, side, qty, price):
+    def ModifyLimitOrder(self, exchangeName, symbol, side, qty, price):
         NotImplementedError("openlimitorder")
 
-
-    def openmarketorder(self, exchangeName, symbol, side, qty):
+    def OpenMarketOrder(self, exchangeName, symbol, side, qty):
         NotImplementedError("openmarketorder")
 
     # 获取平台标准价差
-    def GetStandardDev(exchangeNameA, exchangeNameB,symbol, period, size):
+    def GetStandardDev(self, exchangeNameA, exchangeNameB, symbol, period, size):
         return 0
         # NotImplementedError("getstandarddev"
-        
-    def publish(self,channel,msg):
-        NotImplementedError("getstandarddev")
 
+    def publish(self, channel, msg):
+        NotImplementedError("getstandarddev")
 
     async def initexchange(self):
         self.redis = await aioredis.create_redis('redis://localhost')
 
-
-    async def subscribe(self,channel,callback):
+    async def subscribe(self, channel, callback):
         res = await self.redis.subscribe(channel)
-        await asyncio.ensure_future(self.reader(res[0],callback))
+        await asyncio.ensure_future(self.reader(res[0], callback))
 
-    async def reader(self, ch,callback):
+    async def reader(self, ch, callback):
         while (await ch.wait_message()):
             data = await ch.get_json()
             ch_name = ch.name.decode('utf-8')
-            callback(ch_name,data)
+            callback(ch_name, data)
--- Strategy.py
+++ Strategy.py
@@ -15,30 +15,31 @@ qtyRate = 0.3
 market = {}
 
 # ====================handler===========================
+
+
 class Strategy(object):
-    def __init__(self, es):
+    def __init__(self):
+        es = ExchangeService()
         self.es = es
 
-    def OrderbookChangeHandler(self,orderbook):
+    def OrderbookChangeHandler(self, orderbook):
         position = market["position"]
         # 如果当前交易所有没有仓位
         if(position.qty == 0):
             # 双向开仓
-            OpenPosition(orderbook, "sell")
-            OpenPosition(orderbook, "buy")
+            self.OpenPosition(orderbook, "sell")
+            self.OpenPosition(orderbook, "buy")
         else:
             # 先平仓
-            ClosePosition(orderbook)
+            self.ClosePosition(orderbook)
 
-
-    def OrderChangeHandler(order):
+    def OrderChangeHandler(self, order):
         # 如果限价交易订单完成，则在另外一个交易所反向市价套保
         if(order.ordStatus == "Filled"):
             side = "sell" if order.side == "buy" else "buy"
             self.es.OpenMarketOrder(exchangeB, symbol, side, order.qty)
 
-
-    def PositionChangeHandler(position):
+    def PositionChangeHandler(self, position):
         # 保存仓位信息
         market["position"] = position
 
@@ -46,21 +47,24 @@ class Strategy(object):
     # 根据orderbook数据计算对手交易所的限价单开仓价格与数量
     # orderbook.bid1 为最接近盘口的交易买单
     # orderbook.ask1 为最接近盘口的交易卖单
-    def GetLimitOrderPair(orderbook, side):
+    def GetLimitOrderPair(self, orderbook, side):
         price = 0
         qty = 0
         fees = (Afees * orderbook.bid1.price) + (Bfess * orderbook.bid1.price)
-        standarddev = self.es.GetStandardDev(exchangeA,exchangeB,symbol,'Min',60)
+        standarddev = self.es.GetStandardDev(
+            exchangeA, exchangeB, symbol, "Min", 60)
         if(side == "sell"):
-            price = orderbook.bid1.price * (minRate + 1) + fees * 2 + standarddev
+            price = orderbook.bid1.price * \
+                (minRate + 1) + fees * 2 + standarddev
             qty = orderbook.bid1.qty * qtyRate
         else:
-            price = orderbook.ask1.price * (1 - minRate) - fees * 2 + standarddev
+            price = orderbook.ask1.price * \
+                (1 - minRate) - fees * 2 + standarddev
             qty = orderbook.ask1.qty * qtyRate
         return price, qty
 
     # 根据orderbook计算计算对手交易所的限价平仓订单价格与数量
-    def GetClosePositionOrderPair(orderbook, side):
+    def GetClosePositionOrderPair(self, orderbook, side):
         price = 0
         qty = 0
         if(side == "sell"):
@@ -72,26 +76,30 @@ class Strategy(object):
         return price, qty
 
     # 根据orderbook数据进行开仓操作
-    def OpenPosition(orderbook, side):
-        price, qty = GetLimitOrderPair(orderbook, side)
+    def OpenPosition(self, orderbook, side):
+        price, qty = self.GetLimitOrderPair(orderbook, side)
         if(qty > maxQty):
             qty = maxQty
         self.es.ModifyLimitOrder(exchangeA, symbol, side, qty, price)
 
     # 根据orderbook数据进行平仓操作
-    def ClosePosition(orderbook):
+    def ClosePosition(self, orderbook):
         position = market["position"]
         side = "sell" if position.execQty > 0 else "buy"
-        price, qty = GetClosePositionOrderPair(orderbook, side)
+        price, qty = self.GetClosePositionOrderPair(orderbook, side)
         if(qty > position.qty):
             qty = position.qty
-        self.es.ModifyLimitOrder(exchangeA, symbol, side, qty, price)
+        self.es.modifylimitorder(exchangeA, exchangeA,
+                                 symbol, side, qty, price)
+
+    async def run(self):
+        await self.es.initexchange()
+        await self.es.subscribeorderbook(exchangeB, symbol, self.OrderbookChangeHandler)
+
 
 async def Run():
-    es = ExchangeService(strategy)
-    strategy = Strategy(es)
-    await self.es.initexchange()
-    await self.es.subscribeorderbook(exchangeB, symbol, strategy.orderbookchangehandler)
+    strategy = Strategy()
+    await strategy.run()
     # self.es.subscribeposition(exchangeA, symbol, positionchangehandler)
     # self.es.subscribeorderchange(exchangeA, symbol, orderchangehandler)
 asyncio.run(Run())
