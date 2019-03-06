e50f8184d4d5ae151e9aefa7c74d8efb0bbd4365
ray <ray@terraquanta.io>
2019-03-06 13:47:49 +0800

    ~~~

--- og_strategy.py
+++ og_strategy.py
@@ -1,4 +1,8 @@
 
+from time import sleep
+import datetime
+import sys
+import pprint
 from market_maker.bitmex_mon_api import  BitMexMon
 
 
@@ -10,11 +14,17 @@ def orderBookL2_callback(data):
     print("In orderbookL2 handle!!")
     print(data)
 
+def quote_callback(data):
+    for i in range(len(data)):
+        data[i]['timestamp'] = datetime.datetime.strptime(data[i]['timestamp'], '%Y-%m-%dT%H:%M:%S.%fZ').timestamp()*1000
+    pprint.pprint(data)
+
 def run() -> None:
     bitmex_mon = BitMexMon('XBTUSD')
 
     # Try/except just keeps ctrl-c from printing an ugly stacktrace
-    bitmex_mon.subscribe_data_callback('orderBookL2',orderBookL2_callback,orderBookL2_data_format_func)
+    # bitmex_mon.subscribe_data_callback('orderBookL2',orderBookL2_callback,orderBookL2_data_format_func)
+    bitmex_mon.subscribe_data_callback('quote',quote_callback,None)
     try:
         while True:
             sleep(3)
--- tii_strategy.py
+++ tii_strategy.py
@@ -1,7 +1,23 @@
+from time import sleep
+import sys
+from market_maker.bitmex_mon_api import  BitMexMon
 
 
+def orderBookL2_data_format_func(data):
+    print("In orderbookL2 data_format_func handle!!")
+    return data[:6]+data[-6:]
 
-from market_maker import tii_market_maker
+def orderBookL2_callback(data):
+    print("In orderbookL2 handle!!")
+    print(data)
 
-if __name__ == "__main__":
-    tii_market_maker.run()
+def run() -> None:
+    bitmex_mon = BitMexMon('XBTUSD')
+
+    # Try/except just keeps ctrl-c from printing an ugly stacktrace
+    bitmex_mon.subscribe_data_callback('orderBookL2',orderBookL2_callback,orderBookL2_data_format_func)
+    try:
+        while True:
+            sleep(3)
+    except (KeyboardInterrupt, SystemExit):
+        sys.exit()
