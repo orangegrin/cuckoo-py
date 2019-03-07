26ad681a2e99d2b19bcb3561c1b4f905e45ca1ac
liuqiba <874740127@qq.com>
2019-03-07 18:14:04 +0800

    修改main使用枚举和结构体

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import asyncio
 from exchange.service import ExchangeService
 from exchange.enums import Side
+from exchange.enums import OrderType
+from exchange.enums import OrderResultType
 
 exchange_a = "bitmex"
 exchange_b = "huobi"
@@ -24,57 +26,71 @@ class Strategy(object):
 
     def orderbook_change_handler(self, orderbook):
         print(orderbook)
-        # position = market["position"]
-        # # 如果当前交易所有没有仓位
-        # if(position != None and position.qty == 0):
-        #     # 双向开仓
-        #     self.OpenPosition(orderbook, "sell")
-        #     self.OpenPosition(orderbook, "buy")
-        # else:
-        #     # 先平仓
-        #     self.ClosePosition(orderbook)
 
-    def order_change_handler(self, order):
+        position = market["position"]
+        # 如果当前交易所有没有仓位
+        if(position != None and position.qty == 0):
+            # 双向开仓
+            self.open_position(orderbook, Side.Buy)
+            self.open_position(orderbook, Side.Sell)
+        else:
+            # 先平仓
+            self.close_position(orderbook)
+
+    def order_change_handler(self, order_request):
+        """order_request: ExchangeOrderRequest"""
         # 如果限价交易订单完成，则在另外一个交易所反向市价套保
-        if(order.ordStatus == "Filled"):
-            side = "sell" if order.side == "buy" else "buy"
-            self.es.open_market_order(exchange_b, symbol_b, side, order.qty)
+        if(order_request.orderType == OrderResultType.Filled):
+            side = Side.Sell if order_request.side == Side.Buy else Side.Buy
+            self.es.open_market_order(exchange_b, symbol_b, side, order_request.qty)
 
     def position_change_handler(self, position):
         # 保存仓位信息
         market["position"] = position
 
-    # =====================================================
-    # 根据orderbook数据计算对手交易所的限价单开仓价格与数量
-    # orderbook.bid1 为最接近盘口的交易买单
-    # orderbook.ask1 为最接近盘口的交易卖单
+
     def get_limit_order_pair(self, orderbook, side):
+        """
+            # =====================================================
+            # 根据orderbook数据计算对手交易所的限价单开仓价格与数量
+            # orderbook.bid1 为最接近盘口的交易买单
+            # orderbook.ask1 为最接近盘口的交易卖单
+        """
         price = 0
         qty = 0
-        fees = (a_fees * orderbook.bid1.price) + \
-            (b_fess * orderbook.bid1.price)
+
+        fees = (a_fees * orderbook.bids[0].price) + \
+            (b_fess * orderbook.bids[0].price)
+        # symbol_a- exchange_b
         standarddev = self.es.get_standard_dev(
+
             exchange_a, exchange_b, symbol_a,symbol_b, "Min", 60)
-        if(side == "sell"):
-            price = orderbook.bid1.price * \
+
+        if(side == Side.Sell):
+            price = orderbook.bids[0].price * \
                 (min_rate + 1) + fees * 2 + standarddev
-            qty = orderbook.bid1.qty * qty_rate
+
+            qty = orderbook.bids[0].qty * qty_rate
         else:
-            price = orderbook.ask1.price * \
+
+            price = orderbook.asks[0].price * \
                 (1 - min_rate) - fees * 2 + standarddev
-            qty = orderbook.ask1.qty * qty_rate
+
+            qty = orderbook.asks[0].qty * qty_rate
         return price, qty
 
     # 根据orderbook计算计算对手交易所的限价平仓订单价格与数量
     def get_close_position_order_pair(self, orderbook, side):
         price = 0
         qty = 0
-        if(side == "sell"):
-            price = orderbook.bid1.price
-            qty = orderbook.bid1.qty * qty_rate
+       
+        if(side == Side.Sell):
+            price = orderbook.bids[0].price
+            qty = orderbook.bids[0].qty * qty_rate
         else:
-            price = orderbook.ask1.price
-            qty = orderbook.ask1.qty * qty_rate
+          
+            price = orderbook.asks[0].price
+            qty = orderbook.asks[0].qty * qty_rate
         return price, qty
 
     # 根据orderbook数据进行开仓操作
@@ -87,7 +103,7 @@ class Strategy(object):
     # 根据orderbook数据进行平仓操作
     def close_position(self, orderbook):
         position = market["position"]
-        side = "sell" if position.execQty > 0 else "buy"
+        side = Side.Sell if position.qty> 0 else Side.Buy
         price, qty = self.get_close_position_order_pair(orderbook, side)
         if(qty > position.qty):
             qty = position.qty
