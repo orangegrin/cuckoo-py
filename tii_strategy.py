36e4c87c800f6abef660b1eccb64f4cc4d5a5e0f
ray <ray@terraquanta.io>
2019-03-07 11:57:40 +0800

    ...

--- tii_strategy.py
+++ tii_strategy.py
@@ -184,7 +184,7 @@ def run() -> None:
                             order_cate=1
                             break
                         elif o['side'] == "Buy" if hold_order['IsBuy'] else "Sell":
-                            o['orderID']= hold_order['orderID']
+                            o['orderID']= hold_order['OrderID']
                             order_cate=0
                             break
 
