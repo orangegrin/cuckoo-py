cbc01bb6d714720be9ea6f6fa05d11cf01a9ca0b
rayleeafar <rayleeafar@gmail.com>
2019-03-08 18:06:49 +0800

    .gitignore fix

--- exchange/bitmex/settings.py
+++ exchange/bitmex/settings.py
@@ -1,10 +1,13 @@
-from os.path import join
+# from os.path import join
 import os
-from pathlib import Path,PurePath
+# from pathlib import Path,PurePath
 import logging
 import configparser
 
-prj_root = PurePath(os.path.abspath(__file__)).parents[2]
-cong_path = PurePath(prj_root,'config.ini')
+prj_root = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, os.pardir))
+# prj_root = PurePath(os.path.abspath(os.path.join(__file__, os.pardir))).parents[3]
+conf_path = os.path.join(prj_root,'config.ini')
 settings = configparser.ConfigParser()
-settings.read(cong_path)
+settings.read(conf_path)
+
+# print(conf_path)
